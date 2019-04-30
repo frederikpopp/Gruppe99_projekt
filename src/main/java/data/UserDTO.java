a88bb74773b7dbfd6f7bb2ff67363103267c4e41
Benjamin Bruun <36408665+BenjaminBruun@users.noreply.github.com>
2019-04-30 10:50:04 +0200

    Updatering af UserDTO

--- src/main/java/data/UserDTO.java
+++ src/main/java/data/UserDTO.java
@@ -22,52 +22,15 @@ public class UserDTO implements IUserDTO {
         this.userId = userId;
     }
     @Override
-    public String getUserName() {
-        return userName;
+    public String getRole() {
+        return role;
     }
     @Override
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setRole(String role) {
+        this.role = role;
     }
-    @Override
-    public String getIni() {
-        return ini;
-    }
-    @Override
-    public void setIni(String ini) {
-        this.ini = ini;
-    }
-
-    @Override
-    public List<String> getRoles() {
-        return roles;
-    }
-    @Override
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
-
-    @Override
-    public void addRole(String role){
-        this.roles.add(role);
-    }
-    /**
-     *
-     * @param role
-     * @return true if role existed, false if not
-     */
-    @Override
-    public boolean removeRole(String role){
-        return this.roles.remove(role);
-    }
-
     @Override
     public String toString() {
         return "UserDTO [userId=" + userId + ", userName=" + userName + ", ini=" + ini + ", roles=" + roles + "]";
     }
-
-
-
-
-
 }
