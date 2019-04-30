79d4ff627d0310c3543580ef9b5e9877b9d7f0ec
Frederik David Damsgaard Popp <s174878@student.dtu.dk>
2019-04-30 12:56:11 +0200

    Added static class to create connection
    Instead of creating the private method in each DTO

---
+++ src/main/java/Connection/ConnectionHandler.java
@@ -0,0 +1,11 @@
+package Connection;
+
+import java.sql.*;
+
+public static class ConnectionHandler {
+
+  public static Connection createConnection() throws SQLException {
+      return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s174878?"
+              + "user=s174878&password=oQmcxREHUVWvN9gai9C4K");
+  }
+}
--- src/main/java/User/UserDAO.java
+++ src/main/java/User/UserDAO.java
@@ -3,13 +3,9 @@ package User;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import Connection.ConnectionHandler;
 
 public class UserDAO implements IUserDAO{
-
-    private Connection createConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s174878?"
-                + "user=s174878&password=oQmcxREHUVWvN9gai9C4K");
-    }
 
     @Override
     public void createUser(IUserDTO user) throws DALException {
