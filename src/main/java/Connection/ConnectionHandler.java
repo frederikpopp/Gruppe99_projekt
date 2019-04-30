f927317fcf7f091d4311ae424f8a97d5336328d1
Frederik David Damsgaard Popp <s174878@student.dtu.dk>
2019-04-30 13:10:50 +0200

    Updated connectionhandler
    Cannot have static class, only static methods

--- src/main/java/Connection/ConnectionHandler.java
+++ src/main/java/Connection/ConnectionHandler.java
@@ -2,7 +2,7 @@ package Connection;
 
 import java.sql.*;
 
-public static class ConnectionHandler {
+public class ConnectionHandler {
 
   public static Connection createConnection() throws SQLException {
       return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s174878?"
