785d4fd4acfa570c60d63940ecfce1524d4461a6
Benjamin Bruun <36408665+BenjaminBruun@users.noreply.github.com>
2019-04-30 13:36:38 +0200

    Main test opdatering

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,27 +10,36 @@ import java.util.logging.Logger;
 
 public class Main
 {
-    public static void main(String[] args) {
-      UserDAO DB = new UserDAO();
-      List<IUserDTO> users = new ArrayList<>();
-      List<IUserDTO> persons = new ArrayList<>();
-      UserDTO person = new UserDTO();
-      person.setUserID(1234);
-      person.setRole("Projektleder");
-      person.setAdminStatus(0);
-      try {
-          DB.createUser(person);
+    public static void main(String[] args)
+    {
+        UserDAO DB = new UserDAO();
+        List<IUserDTO> users = new ArrayList<>();
+        List<IUserDTO> persons = new ArrayList<>();
 
-          persons = DB.getUserList();
+        System.out.println(" ----- TEST STARTING -----");
 
-          for (int i = 0; i < persons.size(); i++) {
-              System.out.println(persons.get(i).toString());
-          }
-          //person = DB.getUser(1);
-          //System.out.println(person.toString());
+        UserDTO person = new UserDTO();
+        person.setUserID(2222);
+        person.setRole("Fool");
+        person.setAdminStatus(0);
 
-      } catch (IUserDAO.DALException ex) {
-          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-      }
+        UserDTO person1 = new UserDTO();
+        person.setUserID(4444);
+        person.setRole("Goat");
+        person.setAdminStatus(1);
+        try {
+            DB.createUser(person);
+            DB.createUser(person1);
+
+            persons = DB.getUserList();
+
+            for (int i = 0; i < persons.size(); i++) {
+                System.out.println(persons.get(i).toString());
+            }
+        } catch (IUserDAO.DALException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        System.out.println(" -----  END OF TEST  -----");
     }
 }
