408aa893c43ef01ceb5ef3209ec75a7493f97697
Frederik David Damsgaard Popp <s174878@student.dtu.dk>
2019-04-30 13:05:27 +0200

    Added test to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Gruppe 99!");
-        System.out.println("Hello Panther Pharma!");
-        System.out.println("Suh dude");
-        System.out.println("Testing 123");
+      UserDAOImpls174878 DB = new UserDAOImpls174878();
+      List<UserDTO> persons = new ArrayList<>();
+      UserDTO person = new UserDTO();
+      person.setUserId(1234);
+      person.setUserName("Saxo");
+      person.setIni("SP");
+      //person.addRole("Class President");
+      try {
+          DB.createUser(person);
+
+          persons = DB.getUserList();
+
+          for (int i = 0; i < persons.size(); i++) {
+              System.out.println(persons.get(i).toString());
+          }
+          //person = DB.getUser(1);
+          //System.out.println(person.toString());
+
+      } catch (IUserDAO.DALException ex) {
+          Logger.getLogger(UseDB.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
 }
