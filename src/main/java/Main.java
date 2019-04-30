0685d0e88628e64fc525f4a5f32d19c5c64f2ec4
RobertGronlundN <35996522+RobertGronlundN@users.noreply.github.com>
2019-04-30 09:24:47 +0200

    Panther test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Gruppe 99!");
         System.out.println("Hello Panther Pharma!");
-
+        System.out.println("Suh dude");
     }
 }
