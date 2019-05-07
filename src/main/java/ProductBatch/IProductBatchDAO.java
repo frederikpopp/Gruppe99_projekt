60ef33988fbf927c2895651383e3761dd20695d2
luka0102 <luka0102@live.dk>
2019-05-07 12:19:57 +0200

    Rettet I FEJL

--- src/main/java/ProductBatch/IProductBatchDAO.java
+++ src/main/java/ProductBatch/IProductBatchDAO.java
@@ -4,4 +4,8 @@ import Utilities.DAO;
 
 public interface IProductBatchDAO extends DAO {
 
+  IProductBatchDTO getProductBatches(int productBacthID) throws DALException;
+
+  List<IProductBatchDTO> getAllProductBatches() throws DALException;
+
 }
--- src/main/java/ProductBatch/ProductBatchDAO.java
+++ src/main/java/ProductBatch/ProductBatchDAO.java
@@ -11,7 +11,7 @@ public class ProductBatchDAO implements IProductBatchDAO{
     @Override
     public List<IProductBatchDTO> getAllProductBatches() throws DALException {
         List<IProductBatchDTO> batchList = new ArrayList<>();
-        try(connection c = createConnection()) {
+        try(Connection c = createConnection()) {
             PreparedStatement stmt = c.prepareStatement("SELECT * FROM productbatch");
             ResultSet results = stmt.executeQuery();
             while(results.next()){
@@ -23,14 +23,16 @@ public class ProductBatchDAO implements IProductBatchDAO{
             }
 
             for (IProductBatchDTO i : batchList) {
-                stmt = c.prepareStatement("SELECT * FROM ordered_product WHERE p_batch_ID = i.getBatchID");
+                stmt = c.prepareStatement("SELECT * FROM ordered_product WHERE p_batch_ID = ?");
+                stmt.setInt(1, i.getBatchID());
                 results = stmt.executeQuery();
                 if(results.next()){
                   i.setBatchStatus("Ordered");
                   i.setOrderDate(results.getTimestamp("date_ordered"));
                 }
                 else{
-                  stmt = c.prepareStatement("SELECT * FROM progressing_product WHERE p_batch_ID = i.getBatchID");
+                  stmt = c.prepareStatement("SELECT * FROM progressing_product WHERE p_batch_ID = ?");
+                  stmt.setInt(1, i.getBatchID());
                   results = stmt.executeQuery();
                   if(results.next()){
                       i.setBatchStatus("Progressing");
@@ -38,7 +40,8 @@ public class ProductBatchDAO implements IProductBatchDAO{
                       i.setBeginDate(results.getTimestamp("date_begun"));
                   }
                   else{
-                    stmt = c.prepareStatement("SELECT * FROM finished_product WHERE p_batch_ID = i.getBatchID");
+                    stmt = c.prepareStatement("SELECT * FROM finished_product WHERE p_batch_ID = ?");
+                    stmt.setInt(1, i.getBatchID());
                     results = stmt.executeQuery();
                     if(results.next()){
                         i.setBatchStatus("Finished");
@@ -53,10 +56,61 @@ public class ProductBatchDAO implements IProductBatchDAO{
                 }
             }
 
+        }catch(SQLException e) {
+            throw new DALException(e.getMessage());
         }
         return batchList;
     }
 
     @Override
-    
+    public IProductBatchDTO getProductBatches(int productBacthID) throws DALException {
+      IProductBatchDTO pb = new ProductBatchDTO();
+      try(Connection c = createConnection()) {
+          PreparedStatement stmt = c.prepareStatement("SELECT * FROM productbatch");
+          ResultSet results = stmt.executeQuery();
+          if(results.next()){
+              pb.setBatchID(results.getInt("p_batch_ID"));
+              pb.setRecipeID(results.getInt("recipeID"));
+              pb.setBatchAmount(results.getInt("amount"));
+
+              stmt = c.prepareStatement("SELECT * FROM ordered_product WHERE p_batch_ID = ?");
+              stmt.setInt(1, pb.getBatchID());
+              results = stmt.executeQuery();
+              if(results.next()){
+                pb.setBatchStatus("Ordered");
+                pb.setOrderDate(results.getTimestamp("date_ordered"));
+              }
+              else{
+                stmt = c.prepareStatement("SELECT * FROM progressing_product WHERE p_batch_ID = ?");
+                stmt.setInt(1, pb.getBatchID());
+                results = stmt.executeQuery();
+                if(results.next()){
+                    pb.setBatchStatus("Progressing");
+                    pb.setOrderDate(results.getTimestamp("date_ordered"));
+                    pb.setBeginDate(results.getTimestamp("date_begun"));
+                }
+                else{
+                  stmt = c.prepareStatement("SELECT * FROM finished_product WHERE p_batch_ID = ?");
+                  stmt.setInt(1, pb.getBatchID());
+                  results = stmt.executeQuery();
+                  if(results.next()){
+                      pb.setBatchStatus("Finished");
+                      pb.setOrderDate(results.getTimestamp("date_ordered"));
+                      pb.setBeginDate(results.getTimestamp("date_begun"));
+                      pb.setDoneDate(results.getTimestamp("date_finished"));
+                  }
+                  else{
+                      pb.setBatchStatus("Not Found");
+                  }
+                }
+              }
+          }
+
+      }catch(SQLException e) {
+          throw new DALException(e.getMessage());
+      }
+      return pb;
+    }
+
+
 }
