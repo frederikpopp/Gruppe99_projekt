b094fca5760589c59d39faf2202e3c06812fb472
Frederik David Damsgaard Popp <s174878@student.dtu.dk>
2019-05-07 11:26:30 +0200

    Update CommandModule.java

--- src/main/java/CommandModule/CommandModule.java
+++ src/main/java/CommandModule/CommandModule.java
@@ -1,3 +1,18 @@
 package CommandModule;
 
-import java.main.*;
+import Ingredient.*;
+import ProductBatch.*;
+import Recipe.*;
+import ResourceBatch.*;
+import User.*;
+
+public class CommandModule {
+    IIngredientDAO ingrDAO;
+    IProductBatchDAO pBatchDAO;
+    IProductContentsDAO pContentsDAO;
+    IRecipeDAO recDAO;
+    IRecipeContentsDAO recContentsDAO;
+
+
+
+}
