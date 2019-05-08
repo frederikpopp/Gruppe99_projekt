a040d5279225b0b0b40b8739f10c6f8ad6eeb8f1
RobertGronlundN <35996522+RobertGronlundN@users.noreply.github.com>
2019-05-08 20:56:38 +0200

    test skabeloner

---
+++ src/main/java/Test/CommandModuleTest.java
@@ -0,0 +1,64 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandModuleTest {
+
+    @Test
+    void adminstratorCreateUser() {
+    }
+
+    @Test
+    void adminstratorUpdateUser() {
+    }
+
+    @Test
+    void adminstratorDeleteUser() {
+    }
+
+    @Test
+    void adminstratorGetUser() {
+    }
+
+    @Test
+    void farmaCreateRecipe() {
+    }
+
+    @Test
+    void farmaUpdateRecipe() {
+    }
+
+    @Test
+    void farmaDeleteRecipe() {
+    }
+
+    @Test
+    void farmaGetRecipe() {
+    }
+
+    @Test
+    void farmaGetRecipe1() {
+    }
+
+    @Test
+    void leaderCreateBatch() {
+    }
+
+    @Test
+    void searchBatch() {
+    }
+
+    @Test
+    void searchBatchStatus() {
+    }
+
+    @Test
+    void statusTable() {
+    }
+
+    @Test
+    void labtechUpdateStatus() {
+    }
+}
---
+++ src/main/java/Test/ProductBatchDAOTest.java
@@ -0,0 +1,44 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductBatchDAOTest {
+
+    @Test
+    void getAllProductBatches() {
+    }
+
+    @Test
+    void getProductBatch() {
+    }
+
+    @Test
+    void updateProductBatch() {
+    }
+
+    @Test
+    void deleteProductBatch() {
+    }
+
+    @Test
+    void orderBatch() {
+    }
+
+    @Test
+    void beginBatch() {
+    }
+
+    @Test
+    void finishBatch() {
+    }
+
+    @Test
+    void getAllProductBatchesWhere() {
+    }
+
+    @Test
+    void createProductBatch() {
+    }
+}
