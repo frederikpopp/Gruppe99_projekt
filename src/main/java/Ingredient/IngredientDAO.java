4537279f53bf64964fb2ad3f263f4bdbff2c2432
Asbjørn <s153087@student.dtu.dk>
2019-05-09 21:37:15 +0200

    Making product batch dao test

--- src/main/java/Ingredient/IngredientDAO.java
+++ src/main/java/Ingredient/IngredientDAO.java
@@ -19,7 +19,7 @@ public class IngredientDAO implements IIngredientDAO{
       stmt.executeUpdate();
     } catch (SQLException e) {
       throw new DALException(e.getMessage());
-    }
+  }
   }
 
   @Override
--- src/main/java/Test/ProductBatchDAOTest.java
+++ src/main/java/Test/ProductBatchDAOTest.java
@@ -1,10 +1,58 @@
 package Test;
 
+import ProductBatch.*;
+
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
+
 class ProductBatchDAOTest {
+
+    IProductBatchDAO productbatchDAO = new ProductBatchDAO();
+
+    @Test
+    void jUnitRecipe(){
+        try{
+
+            IProductBatchDTO testProductBatch = new ProductBatchDTO();
+            /*testProductBatch.setBatchID(4);
+            testProductBatch.setRecipeID(2);
+            productbatchDAO.createProductBatch(testProductBatch);
+            productbatchDAO.orderBatch(4);*/
+
+            testProductBatch = productbatchDAO.getProductBatch(4);
+            IProductBatchDTO receivedProductBatch = productbatchDAO.getProductBatch(4);
+            assertEquals(testProductBatch.getRecipeID(), receivedProductBatch.getRecipeID());
+            assertEquals(testProductBatch.getOrderDate(), receivedProductBatch.getOrderDate());
+
+            productbatchDAO.beginBatch(4);
+            testProductBatch = productbatchDAO.getProductBatch(4);
+            receivedProductBatch = productbatchDAO.getProductBatch(4);
+            assertEquals(testProductBatch.getBeginDate(), receivedProductBatch.getBeginDate());
+
+            testProductBatch.setRecipeID(1);
+            productbatchDAO.updateProductBatch(testProductBatch);
+
+            productbatchDAO.finishBatch(4);
+            testProductBatch = productbatchDAO.getProductBatch(4);
+            receivedProductBatch = productbatchDAO.getProductBatch(4);
+            assertEquals(testProductBatch.getRecipeID(), receivedProductBatch.getRecipeID());
+            assertEquals(testProductBatch.getDoneDate(), receivedProductBatch.getDoneDate());
+
+            productbatchDAO.deleteProductBatch(4, "Finished");
+
+
+        }
+        catch (Utilities.DAO.DALException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
 
     @Test
     void getAllProductBatches() {
